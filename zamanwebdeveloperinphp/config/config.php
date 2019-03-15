80ac163fec1796f142ba8745fa3147f0b515c6c8
Syed Zaman Mostafiz <zamanwebdeveloper@gmail.com>
2019-03-15 19:40:44 +0600

    E-Commerce Project Cloning Completed

--- zamanwebdeveloperinphp/config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "db_zamanwebdeveloper");
-define("TITLE", "Zaman Web Developer");
-define("KEYWORDS", "Zaman Web Developer, HTML, CSS, JavaScript, PHP, Bootstrap, Laravel");
